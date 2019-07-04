19d1f8512743c4ed836c17501492af6edeca7925
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2019-07-04 11:32:44 +0300

    add quick runner

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stack exec -- shake-watch -p "site" "site --preview"
+stack exec -- shake-watch -c -p "~/work/projects-hs/migamake/migamake-site/" "~/work/projects-hs/migamake/migamake-site/.stack-work/install/x86_64-linux-tinfo6/lts-11.22/8.2.2/bin/site"
