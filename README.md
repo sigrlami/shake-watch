9d0355b0e40d604c11a617948af9b057f3e5e1ff
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2019-07-04 17:49:34 +0300

    refactor config type usage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # shake-watch
+
+Watch and rebuild functionality for Shake projects.
--- app/Main.hs
+++ app/Main.hs
@@ -27,6 +27,7 @@ import           Distribution.PackageDescription.Parsec        (readGenericPacka
 import           Distribution.Verbosity                        (silent)
 import           Language.Haskell.Ghcid                        as Ghcid
 import           Options.Applicative
+import           Shake.Development.Watch.Types
 import           System.Directory                              (doesFileExist, getCurrentDirectory,
                                                                 listDirectory)
 import           System.Environment                            (getArgs,
@@ -40,18 +41,7 @@ import           System.Posix.Process
 
 --------------------------------------------------------------------------------
 
-data WatchOpt =
-  WatchOpt
-    { watchPath   :: String    -- ^ path to watch dir, by default current
-    , includePath :: String    -- ^ include particular files when watching dir
-    , excludePath :: String    -- ^ exclude particular files when watching dir
-    , cache       :: Bool      -- ^ use Shake cache functionality from `Development.Shake.Database`
-    , remake      :: Bool      -- ^ delete old database upon startup
-    , watch       :: Bool      -- ^ rerun shake with the database after any changes observed to the included directories
-    , delay       :: Int       -- ^ milliseconds to wait for duplicate events
-    , action      :: [String]  -- ^ command to run
-    } deriving (Show)
-
+-- Parser for standard structure
 watchOpt :: Parser WatchOpt
 watchOpt =
   WatchOpt
